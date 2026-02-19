25bd684a688ce8b7e44999c9a3894a74634561b6
cynthiazheng-xiu <372527899@qq.com>
2026-02-19 13:07:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,10 +95,10 @@ with st.sidebar:
     if "20HQ" in container_type:
         container_volume = 28.0    # 20HQ体积 (CBM)
         container_weight = 22000    # 20HQ限重 (KG)
-    elsif:  # 40HQ
+    elif:  # 40HQ
         container_volume = 67.7     # 40HQ体积 (CBM)
         container_weight = 26000     # 40HQ限重 (KG)
-    elsif:  # 40FQ
+    elif:  # 40FQ
         container_volume = 67.7     # 40FQ体积 (CBM)
         container_weight = 26000     # 40FQ限重 (KG)
     
